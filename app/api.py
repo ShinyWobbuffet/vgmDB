22320bc7f66a60f03d655d7881113d81309badb1
ShinyWobbuffet <63820638+ShinyWobbuffet@users.noreply.github.com>
2021-09-25 18:55:24 -0400

    Create api.py

---
+++ app/api.py
@@ -0,0 +1,19 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+  return {"res": 0}
+
+@app.route("/<hash>")
+def view(identifier):
+  file = open("data.json")
+  data = file.read()
+
+  if data.games[identifier]:
+    return {"res": data.games[identifier]}
+  elif data.songs[identifier]:
+    return {"res": data.songs[identifier]}
+  else:
+    return {"res": -1}
